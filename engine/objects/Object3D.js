17c3023d9d58570f549b061e097213c4ac76f24e
John Hudson <john.hudson@seequestor.com>
2022-10-15 03:24:09 +0100

    Starting creating engine functionality

--- engine/objects/Object3D.js
+++ engine/objects/Object3D.js
@@ -5,6 +5,7 @@ class Object3D {
 	constructor(pathToObject) {
 		this.assetPath = pathToObject;
 		this.mesh = null;
+		this.scripts = [];
 	}
 	load(){
 		return new Promise((resolve , reject) => {
@@ -22,21 +23,11 @@ class Object3D {
 							this.loader = new THREE.OBJLoader();
 							this.mesh = this.loader.parse(obj.mesh);
 
-							for(let prop in obj.material) {
-								AssetManager.loadAsset(
-									obj.material[prop].src
-								)
-								.then((base64) => {
-									 let img = new Image();
-									 let texture = new THREE.Texture(img);
-									 img.onload = () => {
-									 	texture.needsUpdate = true;
-									 };
-									 img.src = 'data:' + obj.material[prop].mime + ';base64,' + base64;
-									 this.mesh.material[prop] = img;
-								});
-							}
-							resolve();
+							this.loadMaterialsAndScripts(obj)
+								.then(() => {
+									resolve();
+								}).catch(reject);
+							
 						}catch(e){
 							reject(e);
 						}
@@ -44,6 +35,45 @@ class Object3D {
 				}
 			});
 		});
+	}
+	loadMaterialsAndScripts(obj){
+		return new Promise((resolve , reject) => {
+			for(let prop in obj.material) {
+				AssetManager.loadAsset(
+					obj.material[prop].src
+				)
+				.then((base64) => {
+					 let img = new Image();
+					 let texture = new THREE.Texture(img);
+					 img.onload = () => {
+					 	texture.needsUpdate = true;
+					 };
+					 img.src = 'data:' + obj.material[prop].mime + ';base64,' + base64;
+					 this.mesh.material[prop] = img;
+				})
+				.catch(reject);
+			}
+			if ( obj.scripts && Array.isArray(obj.scripts) ) {
+				obj.scripts.forEach((script) => {
+					AssetManager.loadAsset(
+						script
+					)
+					.then((base64) => {
+						let js = atob(base64);
+						window.exports = {};
+						eval(js);
+						let obj = window.exports;
+
+						let inst = new obj(this);
+
+						this.scripts.push(inst);
+
+						
+					}).catch(reject);
+				});
+				resolve();
+			}
+		})
 	}
 }
 module.exports = Object3D;
